39612408e4a0aabf43c5c6cce4845d1adff0b548
Miguel A. Pardo Alvarez <miguelpardoalvare50@gmail.com>
2024-09-12 09:59:33 -0500

    Creacion de apps para la separacion del proyecto

---
+++ dispositivos_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DispositivosAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dispositivos_app'
---
+++ usuarios_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsuauriosAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'usuaurios_app'
